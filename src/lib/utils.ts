9e2c737d70cf8f2a7a1bbd208feac5c15f579fcf
lemasani <brianlemasan@gmail.com>
2025-07-18 14:16:28 +0300

    added theme provider

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function transformToSelectOptions<T extends Record<string, any>>(
+  data: T[],
+  labelField: keyof T,
+  valueField: keyof T = 'id' as keyof T
+): { value: string; label: string }[] {
+  return data
+    .filter((item) => item[valueField] !== undefined)
+    .map((item) => ({
+      value: String(item[valueField]),
+      label: String(item[labelField] ?? ''),
+    }));
+}
